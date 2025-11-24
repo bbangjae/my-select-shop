dfe75620da371214574da071e478b9d453be2faf
bbangjae <thebyunjae@naver.com>
2025-11-24 23:44:32 +0900

    feat(back): 카카오 로그인 기능 추가

--- src/main/java/com/example/myselectshop/repository/UserRepository.java
+++ src/main/java/com/example/myselectshop/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.myselectshop.repository;
 
 import com.example.myselectshop.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByKakaoId(Long kakaoId);
 }
--- src/main/java/com/example/myselectshop/service/KakaoService.java
+++ src/main/java/com/example/myselectshop/service/KakaoService.java
@@ -1,11 +1,14 @@
 package com.example.myselectshop.service;
 
 import com.example.myselectshop.dto.KakaoUserInfoDto;
+import com.example.myselectshop.entity.User;
+import com.example.myselectshop.entity.UserRoleEnum;
 import com.example.myselectshop.jwt.JwtUtil;
 import com.example.myselectshop.repository.UserRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -39,6 +42,12 @@ public class KakaoService {
 
         // 2. 토큰으로 카카오 API 호출 : "액세스 토큰"으로 "카카오 사용자 정보" 가져오기
         KakaoUserInfoDto kakaoUserInfo = getKakaoUserInfo(accessToken);
+
+        // 3. 필요시에 회원가입
+        User kakaoUser = registerKakaoUserIfNeeded(kakaoUserInfo);
+
+        // 4. JWT 토큰 반환
+        jwtUtil.createToken(kakaoUser.getUsername(), kakaoUser.getRole());
 
         return null;
     }
@@ -85,5 +94,35 @@ public class KakaoService {
 
         log.info("카카오 사용자 정보: {}, {}, {}", id, nickname, email);
         return new KakaoUserInfoDto(id, nickname, email);
+    }
+
+    private User registerKakaoUserIfNeeded(KakaoUserInfoDto kakaoUserInfo) {
+        // DB 에 중복된 Kakao Id 가 있는지 확인
+        Long kakaoId = kakaoUserInfo.getId();
+        User kakaoUser = userRepository.findByKakaoId(kakaoId).orElse(null);
+
+        if (kakaoUser == null) {
+            // 카카오 사용자 email 동일한 email 가진 회원이 있는지 확인
+            String kakaoEmail = kakaoUserInfo.getEmail();
+            User sameEmailUser = userRepository.findByEmail(kakaoEmail).orElse(null);
+            if (sameEmailUser != null) {
+                kakaoUser = sameEmailUser;
+                // 기존 회원정보에 카카오 Id 추가
+                kakaoUser = kakaoUser.kakaoIdUpdate(kakaoId);
+            } else {
+                // 신규 회원가입
+                // password: random UUID
+                String password = UUID.randomUUID().toString();
+                String encodedPassword = passwordEncoder.encode(password);
+
+                // email: kakao email
+                String email = kakaoUserInfo.getEmail();
+
+                kakaoUser = new User(kakaoUserInfo.getNickname(), encodedPassword, email, UserRoleEnum.USER, kakaoId);
+            }
+
+            userRepository.save(kakaoUser);
+        }
+        return kakaoUser;
     }
 }
